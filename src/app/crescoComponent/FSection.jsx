da0ffe530aef78c27167fff7a1873e808103201b
Fazalwahab12 <wahabdir567@gmail.com>
2024-06-08 10:40:06 -0700

    fix

--- src/app/crescoComponent/FSection.jsx
+++ src/app/crescoComponent/FSection.jsx
@@ -1,8 +1,7 @@
-'use client'
+'use client';
 import React from "react";
+import Head from 'next/head';
 import localFont from "next/font/local";
-import Image from "next/image";
-import DropdownTwo from "@/components/form-control";
 
 // Load fonts with font-display: swap
 const stroma = localFont({ src: "../fonts/Stroma-Regular.ttf", display: 'swap' });
@@ -11,25 +10,23 @@ const roboto = localFont({ src: "../fonts/Roboto-Black.ttf", display: 'swap' });
 const FSection = () => {
   return (
     <>
+      <Head>
+        {/* Preload critical resources */}
+        <link rel="preload" href="/fonts/Stroma-Regular.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        <link rel="preload" href="/fonts/Roboto-Black.ttf" as="font" type="font/ttf" crossOrigin="anonymous" />
+        <link rel="preload" href="/assets/img/bg-cresco.jpg" as="image" />
+      </Head>
       <div
         className="align-items-center d-flex hero-header overflow-hidden position-relative"
         style={{ height: "100vh", width: "100vw" }}
       >
         {/* Start Oblique */}
         <div>
-          {/* <video
-            autoPlay
-            muted
-            loop
-            src="/assets/img/Website.mp4"
-            className="h-100 object-fit-cover position-absolute w-100 oblique-image top-0 video"
-            preload="metadata" // Preload only metadata to start
-          /> */}
-          <Image
+          <img
             src="/assets/img/bg-cresco.jpg"
-            layout="fill"
-            alt="Hero GIF"
-            className="h-100 w-100 object-fit-cover position-absolute  oblique-image top-0 video"
+            alt="Hero Background"
+            className="h-100 object-fit-cover position-absolute w-100 oblique-image top-0"
+            loading="eager" // Ensure the background image loads eagerly
           />
         </div>
         {/* /.End Oblique */}
@@ -38,17 +35,11 @@ const FSection = () => {
           style={{ height: "100%", width: "100%" }}
         >
           <div>
-            <div>
-              <div className="bg-[#E1B958] d-inline-block fw-medium mb-3 rounded-pill section-header__subtitle text-capitalize text-gray-900">
-                Cresco Real Estate LLC
-              </div>
-              <h1 className=" text-white  font-bold  text-5xl ">
-                <span className={stroma.className}>
-                  Unlock The Door <br /> To Your Dream Home
-                </span>
-              </h1>
-              <h2 className="text-white text-2xl"></h2>
-            </div>
+            <h1 className="text-white font-bold text-5xl">
+              <span className={stroma.className}>
+                Unlock The Door <br /> To Your Dream Home
+              </span>
+            </h1>
           </div>
         </div>
       </div>
